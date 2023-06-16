27d96a8ef379a89724055d5331ce3750cc05c604
Mahia <134421469+Mahdiyaaaaaa@users.noreply.github.com>
2023-06-16 19:10:53 +0600

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,3 @@
+#Mahia
+
+### Assignment-4
---
+++ app.js
@@ -0,0 +1,84 @@
+// Background Color
+var bg = document.getElementById("iits-bg");
+bg.classList.remove("bg-danger");
+bg.style.background = "#007BFF";
+
+// Image
+var img = document.getElementById("iits-img");
+img.src = "./img.jpg";
+
+// Copyright Text
+var copy = document.getElementById("iits-cr-msg");
+copy.textContent = "Be like a Flowers";
+
+// Button Color
+var btn = document.getElementById("iits-btn");
+btn.classList.remove("bg-danger");
+btn.style.background = "#007BFF";
+
+// Highlighted Text
+var text1 = document.getElementById("iits-name");
+text1.textContent = "Mahia";
+text1.classList.remove("text-danger");
+text1.style.color = "#007BFF";
+
+var text2 = document.getElementById("iits-prof");
+text2.textContent = "student";
+text2.classList.remove("text-danger");
+text2.style.color = "#007BFF";
+
+var text3 = document.getElementById("iits-edu");
+text3.textContent = "BSc";
+text3.classList.remove("text-danger");
+text3.style.color = "#007BFF";
+
+var text4 = document.getElementById("iits-study");
+text4.textContent = "CSE";
+text4.classList.remove("text-danger");
+text4.style.color = "#007BFF";
+
+var text5 = document.getElementById("iits-school");
+text5.textContent = "IUBAT";
+text5.classList.remove("text-danger");
+text5.style.color = "#007BFF";
+
+var text6 = document.getElementById("iits-passion");
+text6.textContent = "coding";
+text6.classList.remove("text-danger");
+text6.style.color = "#007BFF";
+
+var text7 = document.getElementById("iits-numYears");
+text7.textContent = "two years";
+text7.classList.remove("text-danger");
+text7.style.color = "#007BFF";
+
+var text8 = document.getElementById("iits-hobby");
+text8.textContent = "to do code";
+text8.classList.remove("text-danger");
+text8.style.color = "#007BFF";
+
+var text9 = document.getElementById("iits-crc");
+text9.textContent = "extroverts";
+text9.classList.remove("text-danger");
+text9.style.color = "#007BFF";
+
+var text10 = document.getElementById("iits-ability");
+text10.textContent = "to run on my way";
+text10.classList.remove("text-danger");
+text10.style.color = "#007BFF";
+
+var text11 = document.getElementById("iits-goal");
+text11.textContent = "being a web developer";
+text11.classList.remove("text-danger");
+text11.style.color = "#007BFF";
+
+// Social links
+
+var fb = document.getElementById("iits-fb-link");
+fb.href = "https://www.facebook.com/sumaiyamahdiya.mahia";
+
+var mail = document.getElementById("iits-email");
+mail.href = "mailto:22103118@iubat.edu";
+
+var github = document.getElementById("iits-github-link");
+github.href = "https://github.com/Mahdiyaaaaaa";
